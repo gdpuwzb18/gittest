5b396fc3364d2c3918c765ebe92d996bd520f4ae
wzb <474173225@qq.com>
2022-05-19 09:36:37 +0800

    second commit

--- src/main/java/com/atjava/git/GitTest.java
+++ src/main/java/com/atjava/git/GitTest.java
@@ -3,5 +3,7 @@ package com.atjava.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("Hello GIt!!!");
+        System.out.println("Hello GIt2!!!");
+
     }
 }
